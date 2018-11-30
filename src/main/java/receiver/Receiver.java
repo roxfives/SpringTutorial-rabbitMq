e93250fcfe45a2c28f6255a0e12a7c150daffd13
rchagas <ricardo.chagas2@outlook.com>
2018-11-30 10:05:17 -0200

    Implements the full application

--- src/main/java/receiver/Receiver.java
+++ src/main/java/receiver/Receiver.java
@@ -4,15 +4,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * The class that receives the message from tue queue
+ * */
 @Component
 public class Receiver {
+    // An auxiliary barrier for the threads used to warn the application the message has been received
     private CountDownLatch latch = new CountDownLatch(1);
 
+    /**
+     * The method that should be called to deliver the message to an
+     * instance of this class
+     *
+     * @param message the message to be delivered to the object
+     * */
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
-        latch.countDown();
+        latch.countDown(); // A workaround to warn the application the message has been received
     }
 
+    /**
+     * A simple getter to give access to the auxiliary barrier
+     *
+     * @return    the auxiliary barrier
+     * */
     public CountDownLatch getLatch() {
         return latch;
     }
---
+++ src/main/java/run/Application.java
@@ -0,0 +1,69 @@
+package run;
+
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import receiver.Receiver;
+import sender.Runner;
+
+/**
+ *
+ * */
+@SpringBootApplication(scanBasePackageClasses = {Receiver.class, Runner.class})
+public class Application {
+    static final String topicExchangeName = "spring-boot-application";
+    static final String queueName = "spring-boot";
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Application.class, args).close();
+    }
+
+    public static String getTopicExchangeName() {
+        return Application.topicExchangeName;
+    }
+
+    public static String getQueueName() {
+        return Application.queueName;
+    }
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        System.out.println("Message received from listener: routing to receiver... ");
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+}
